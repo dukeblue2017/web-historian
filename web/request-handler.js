6fe435a45e8175307c4401f3f454bd3d674c4f06
Hack Reactor Student <student@hackreactor.com>
2017-10-26 12:24:17 -0500

    removed outer logic, wrote htmlfetcher.js, progress on writing post handler

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,26 +1,31 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
-// require more modules/folders here!
 var httpHelpers = require('./http-helpers');
 var fs = require('fs');
 
 
 exports.handleRequest = function (request, response) {
-  //on GET
-  if (request.url === '/') {
-    if (request.method === 'GET') {
-      fs.readFile(archive.paths.siteAssets + '/index.html', function(err, data) {
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        response.write(data);
-        response.end(data);
+  if (request.method === 'GET') {
+    console.log('GET received, endpoint: ', request.url);
+    fs.readFile(archive.paths.siteAssets + '/index.html', function(err, data) {
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.write(data);
+      response.end();
+    });
+  } else if (request.method === 'POST') {
+    console.log('POST received, endpoint: ', request.url);
+    request.on('data', function(chunk) {
+      var urlToPost = ('' + chunk).slice(4);
+      archive.isUrlInList(urlToPost, function() {
+        //if yes: 1)if in archive: return archived html 2)if not in archive: loading.html
+        //if no: add urlToPost to list and send them to loading
       });
-    } else {
-      response.writeHead(405, httpHelpers.headers);
-      response.end('405: Method not allowd');
-    }
+    });
+  } else {
+    console.log('Method received is not GET or POST, endpoint: ', request.url);
+    response.writeHead(405, httpHelpers.headers);
+    response.end('405: Method not allowed to endpoint /');
   }
-
-  //on POST 
-  //response.end(archive.paths.list);
+  
 };
 
